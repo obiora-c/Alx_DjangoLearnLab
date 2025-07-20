371683a8ca7b3d8b3c2abebf1a4dd9b8601ff09b
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-07-20 04:06:24 -0700

    CBV

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,10 +8,10 @@ from .models import Book, Library
 
 # Function-based view to list all books
 def list_books(request):
-    books = Book.objects.select_related('author').all()  # Efficiently fetch authors
+    books = Book.objects.all()  # <-- REQUIRED EXPLICITLY
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view to show details of a specific library
+# Class-based view for Library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
