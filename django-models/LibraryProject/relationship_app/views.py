b19a7abc99d431f2ff15ec77a617269a1c038584
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-07-20 03:52:14 -0700

    CBV

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+ #Function-based View: List All Books
+ 
+ 
+from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'list_books.html', {'books': books})
+
+
+#Class-based View: Display Library Details
+
+from django.views.generic.detail import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
