cd03985bc2d4787e84fd1ce82566e4cc547828db
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-07-20 09:54:13 -0700

    user

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -24,3 +24,13 @@ urlpatterns = [
 
 # urls.py
 
+from django.urls import path
+from .admin_view import admin_view
+from .librarian_view import librarian_view
+from .member_view import member_view
+
+urlpatterns = [
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
+]
