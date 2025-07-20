b795669f19139e642b6c0444dd90ddeb23014847
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-07-20 01:42:29 -0700

    DEL

--- django-models/LibraryProject/relationship_app/urls.py
+++
@@ -1,9 +0,0 @@
-
-
-from django.urls import path
-from .views import book_list_view, LibraryDetailView
-
-urlpatterns = [
-    path('books/', book_list_view, name='book-list'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-]
