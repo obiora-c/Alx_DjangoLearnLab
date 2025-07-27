3df84dc03777a8cfb3f7db20bc9ba68ab92e8029
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-07-27 00:37:40 -0700

    customUser

---
+++ advanced_features_and_security/LibraryProject/accounts/admin.py
@@ -0,0 +1,39 @@
+
+
+# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.forms import TextInput, Textarea
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "date_of_birth", "profile_photo")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo"),
+        }),
+    )
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
