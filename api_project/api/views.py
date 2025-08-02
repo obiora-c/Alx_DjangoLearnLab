f3d0d2976931cac8083a0d34b0e73c8c5536a31a
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-08-02 09:18:20 -0700

    AuthenticationandPermission

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,6 +6,9 @@ from rest_framework import generics
 from .models import Book
 from rest_framework import viewsets
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import BasePermission
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -16,3 +19,16 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # ✅ Requires authentication
+    permission_classes = [IsAdminUser]
+    
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.method in ['GET', 'HEAD', 'OPTIONS'] or obj.owner == request.user
+    
+    
+    
+# BookViewSet requires users to be authenticated using token-based auth.
+# Users must send their token in the Authorization header as: Token <token>
+# This view will reject unauthenticated requests.
