407c3ce1161433111a6b2a4eccf2894e636fc0fd
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-08-14 07:36:33 -0700

    UserAuth

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
 
-# Create your views here.
+# blog/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto login after registration
+            return redirect('profile')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get('email')
+        request.user.save()
+        return redirect('profile')
+    return render(request, 'blog/profile.html')
