7bf2592fc484ffef2c8726bff0ce483d719efeed
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-08-16 05:41:17 -0700

    CommentFun

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -21,8 +21,8 @@ urlpatterns = [
     path("post/<int:pk>/",        PostDetailView.as_view(), name="post-detail"),
     path("post/<int:pk>/update/",   PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-    path("posts/<int:post_id>/comments/new/", CommentCreateView.as_view(), name="comment-add"),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-edit'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path("post/<int:post_id>/comments/new/", CommentCreateView.as_view(), name="comment-add"),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
 ]
