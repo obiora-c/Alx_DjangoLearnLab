d2c39a3dd0c0b2f42a2b5f47470afbb883111dcb
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-08-16 07:55:47 -0700

    AdvanceFeature

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget  # <-- this is what you’re missing!
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -20,6 +21,9 @@ class PostForm(forms.ModelForm):
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Post title"}),
             "content": forms.Textarea(attrs={"class": "form-control", "rows": 8, "placeholder": "Write your post..."}),
+        }
+        widgets = {
+            "tags": TagWidget(),  # makes tags input cleaner
         }
 
 
