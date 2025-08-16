d0504086388127159e16fa54975023592ce5d168
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-08-16 05:21:53 -0700

    commentFunct

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -89,19 +89,20 @@ class PostDeleteView(LoginRequiredMixin, AuthorRequiredMixin, DeleteView):
 
 
 
-# Add comment
-##   post = get_object_or_404(Post, id=post_id)
- #   if request.method == "POST":
-  #      form = CommentForm(request.POST)
-   #     if form.is_valid():
-    #        comment = form.save(commit=False)
-     #       comment.post = post
-      #      comment.author = request.user
-       #     comment.save()
-  #          return redirect('post-detail', pk=post.pk)
-   # else:
-    #    form = CommentForm()
-   # return redirect('post-detail', pk=post.pk)
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect("post-detail", pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, "blog/comment_form.html", {"form": form, "post": post})
 
 
 
