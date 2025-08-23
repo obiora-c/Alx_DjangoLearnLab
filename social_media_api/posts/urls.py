306b5bebd5b6fcb36bdaf842e092ed17aec78519
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-08-23 05:09:18 -0700

    Notificationandlikes

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,7 @@
 
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import PostViewSet, CommentViewSet, FeedView
+from .views import PostViewSet, CommentViewSet, FeedView, LikePostView, UnlikePostView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -11,4 +11,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='feed'),
+    path("<int:pk>/like/", LikePostView.as_view(), name="like-post"),
+    path("<int:pk>/unlike/", UnlikePostView.as_view(), name="unlike-post"),
+
 ]
