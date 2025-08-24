a03faf42b1188bac723746c12238e76efd1823f4
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-08-24 15:10:35 -0700

    Heroku

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -80,6 +80,7 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -157,16 +158,7 @@ CSRF_COOKIE_SECURE = True
 import os
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT", "5432"),
-    }
-}
+
 
 
 
