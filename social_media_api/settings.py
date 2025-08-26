b452a45bb38b9fe90773f5cdbfa61cf14ef2eee8
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-08-26 16:19:57 -0700

    Prepare for render deployment

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -23,10 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&jlo9wue##hat)1oohimpsllotd$+^(fi8-)hirauy+qqam2c('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 import os
 ALLOWED_HOSTS = ["social-media-api-k0ry.onrender.com"]
+
+
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
 
 
 # Application definition
